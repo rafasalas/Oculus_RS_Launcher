e9226a8a7c7202d6e8736db5695478d21f3ede4c
Rafael Salas Solis <salas@eprinsa.org>
2023-07-04 19:42:22 +0200

    Initial commit

---
+++ App/src/main/java/com/basti564/dreamgrid/PopupUtils.java
@@ -0,0 +1,28 @@
+package com.basti564.dreamgrid;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class PopupUtils {
+    public static Dialog showPopup(Activity activity, int layout) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+        alertDialogBuilder.setView(layout);
+        AlertDialog dialog = alertDialogBuilder.create();
+        dialog.show();
+
+        WindowManager.LayoutParams windowLayoutParams = dialog.getWindow().getAttributes();
+        windowLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        windowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        windowLayoutParams.gravity = Gravity.END & Gravity.BOTTOM;
+        windowLayoutParams.x = 50;
+        windowLayoutParams.y = 50;
+        dialog.getWindow().setAttributes(windowLayoutParams);
+        dialog.findViewById(R.id.layout).requestLayout();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.bkg_dialog);
+
+        return dialog;
+    }
+}
